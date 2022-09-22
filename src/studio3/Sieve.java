c58864bdffe314307c257503fe504a12836ca7d1
andrewng <andrewng@localhost>
2022-09-22 12:46:51 -0500

    Studio Three

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,20 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("n?");
+		int startingN = in.nextInt();
+		int[] intArray = new int[startingN];
+		for (int i=0; i==startingN; i++)
+		{
+			intArray[i]=startingN;
+		}
+		System.out.println(intArray);
+	}
+
+}
